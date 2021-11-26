6ac58afc8edf92a0950504b851cf4bb4841ed600
James Salvatore <jimmy.c.salvatore@gmail.com>
2021-11-26 00:47:37 -0600

    chore: added obs ws test;

---
+++ obs-ws-test/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <TestComponent/>
+  </div>
+</template>
+
+<script>
+import TestComponent from '@/components/TestComponent.vue';
+
+export default {
+  name: 'App',
+  components: { TestComponent },
+};
+</script>
